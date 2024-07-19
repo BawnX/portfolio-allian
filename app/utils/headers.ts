64ee2524db1722db14b34c5c905e7ee86b06e862
Antonio Morales <antoniomoralesrocha@gmail.com>
2024-07-18 22:43:27 -0400

    feat: 🎸 added some headers

--- app/utils/headers.ts
+++ app/utils/headers.ts
@@ -4,3 +4,31 @@ export function contentSecurityPolicyHeader(response: NextResponse<unknown>) {
   response.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:");
   return response;
 }
+
+export function antiClickjackingHeader(response: NextResponse<unknown>) {
+  response.headers.set('X-Frame-Options', 'DENY');
+  return response;
+}
+
+export function permissionsPolicyHeader(response: NextResponse<unknown>) {
+  response.headers.set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+  return response;
+}
+
+export function xPoweredByHeader(response: NextResponse<unknown>) {
+  response.headers.delete('X-Powered-By');
+  return response;
+}
+
+export function xContentTypeOptionsHeader(response: NextResponse<unknown>) {
+  response.headers.set('X-Content-Type-Options', 'nosniff');
+  return response;
+}
+
+export function secFetchHeader(response: NextResponse<unknown>) {
+  response.headers.set('Sec-Fetch-Dest', 'document');
+  response.headers.set('Sec-Fetch-Mode', 'navigate');
+  response.headers.set('Sec-Fetch-Site', 'same-origin');
+  response.headers.set('Sec-Fetch-User', '?1');
+  return response;
+}
--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,22 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { contentSecurityPolicyHeader } from './app/utils/headers';
+import { antiClickjackingHeader, contentSecurityPolicyHeader, permissionsPolicyHeader, secFetchHeader, xContentTypeOptionsHeader, xPoweredByHeader } from './app/utils/headers';
 
 export function middleware(request: NextRequest) {
   let response = NextResponse.next();
 
   response = contentSecurityPolicyHeader(response);
-
-  // Anti-clickjacking header
-  response.headers.set('X-Frame-Options', 'DENY');
-
-  // Permissions Policy
-  response.headers.set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
-
-  // Remove X-Powered-By header
-  response.headers.delete('X-Powered-By');
-
-  // X-Content-Type-Options header
-  response.headers.set('X-Content-Type-Options', 'nosniff');
+  response = antiClickjackingHeader(response);
+  response = permissionsPolicyHeader(response);
+  response = xPoweredByHeader(response);
+  response = xContentTypeOptionsHeader(response);
+  response = secFetchHeader(response);
 
   // Sec-Fetch headers
-  response.headers.set('Sec-Fetch-Dest', 'document');
-  response.headers.set('Sec-Fetch-Mode', 'navigate');
-  response.headers.set('Sec-Fetch-Site', 'same-origin');
-  response.headers.set('Sec-Fetch-User', '?1');
+
   return response;
 }
 
 export const config = {
-  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  matcher: '/((?!api|_next/image|favicon.ico).*)',
 };
