863919ebabeffaf650e1cc0223e629dd5c2e67a9
Antonio Morales <antoniomoralesrocha@gmail.com>
2024-07-18 21:20:49 -0400

    feat: 🎸 added middleware

---
+++ middleware.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { contentSecurityPolicyHeader } from './app/utils/headers';
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  contentSecurityPolicyHeader(response);
+
+  // Anti-clickjacking header
+  response.headers.set('X-Frame-Options', 'DENY');
+
+  // Permissions Policy
+  response.headers.set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+
+  // Remove X-Powered-By header
+  response.headers.delete('X-Powered-By');
+
+  // X-Content-Type-Options header
+  response.headers.set('X-Content-Type-Options', 'nosniff');
+
+  // Sec-Fetch headersz
+  response.headers.set('Sec-Fetch-Dest', 'document');
+  response.headers.set('Sec-Fetch-Mode', 'navigate');
+  response.headers.set('Sec-Fetch-Site', 'same-origin');
+  response.headers.set('Sec-Fetch-User', '?1');
+
+  return response;
+}
+
+export const config = {
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+};
